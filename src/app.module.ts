9796171e0361cf228ba0f7fa76fdceffc1bf8c6d
Gabriel Fernando <gf313074@gmail.com>
2025-04-11 22:46:34 -0300

    adjust imports to authmodule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AuthController } from './auth/auth.controller';
-import { AuthService } from './auth/auth.service';
+
 
 @Module({
   imports: [],
-  controllers: [AuthController],
-  providers: [AuthService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
